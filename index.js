4ac55746d6e2a8ce63622265d4862a9d55bf2743
Dylan Michael Labat <dml252@live.com>
2019-08-15 20:51:24 -0500

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,27 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation(){
+  return document.querySelector('input').value
+}
+
+function addNewElementAsLi(){
+  let list = document.createElement('li')
+  list.innerHTML = retrieveEmployeeInformation()
+  return document.querySelector('.employee-list').appendChild(list)
+}
+
+function addNewLiOnClick(){
+  let click = document.querySelector('input')
+  click.addEventListener("click", addNewElementAsLi())
+  click.value = ''
+}
+
+function clearEmployeeListOnLinkClick(){
+  let a = document.querySelector('a')
+  let ul = document.querySelector('ul')
+  a.addEventListener('click', function(){
+    ul.innerHTML = ''
+  })
+}
