43c8e86681acaec351c9c1c368b6381d34ab1498
imaglazer <avigayilglazer@gmail.com>
2019-08-18 20:07:58 +0300

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
 function preventRefreshOnSubmit(){
     document.querySelector('form').addEventListener('submit', function(event){
       event.preventDefault()
+
     })
 }
 
 preventRefreshOnSubmit()
+
+const input = document.querySelector('input')
+
+function retrieveEmployeeInformation() {
+  return input.value
+}
+
+function addNewElementAsLi() {
+  let list = document.querySelector('.employee-list')
+  let employee = retrieveEmployeeInformation()
+  let li = document.createElement('li')
+  li.innerHTML = employee
+
+  return list.appendChild(li)
+}
+
+function addNewLiOnClick() {
+  let submit = document.querySelector('input[type="submit"]')
+  submit.addEventListener('click', function(event) {
+    addNewElementAsLi()
+    document.querySelector('input').value = ""
+  })
+}
+
+function clearEmployeeListOnLinkClick() {
+  let button = document.querySelector('a')
+  button.addEventListener('click', function(event) {
+    return document.querySelector('ul').innerHTML = ""
+  })
+}
